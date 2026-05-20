3ce0a6af995a0a4d7a72a88a048f1cd7d34d89c4
Armaan Singh <armaansingh2048@gmail.com>
2026-05-20 18:39:00 +0530

    Setup Jest coverage configuration

---
+++ jest.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: "jsdom",
+
+  collectCoverage: true,
+
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+
+  coverageDirectory: "coverage",
+
+  coverageReporters: ["text", "html"],
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
